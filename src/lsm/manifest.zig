2441324d1776be3da3a285369b89fa986a0b6473
Brian Anderson <andersrb@gmail.com>
2024-06-04 16:29:04 -0600

    Add value_count_max_actual

--- src/lsm/manifest.zig
+++ src/lsm/manifest.zig
@@ -28,6 +28,8 @@ pub fn TreeTableInfoType(comptime Table: type) type {
     return struct {
         const TreeTableInfo = @This();
 
+        pub const value_count_max_actual = Table.value_count_max_actual;
+
         /// Checksum of the table's index block.
         checksum: u128,
         /// Address of the table's index block.
