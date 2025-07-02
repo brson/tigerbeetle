d69e2afdde09c3eb556578a35bb963f1307c0321
Brian Anderson <andersrb@gmail.com>
2025-07-02 15:44:26 -0400

    tb_client: Always take the lock when accessing the submitted queue

--- src/clients/c/tb_client/context.zig
+++ src/clients/c/tb_client/context.zig
@@ -199,6 +199,8 @@ pub fn ContextType(
         completion_context: usize,
 
         interface: *ClientInterface,
+        // The submission queue. This is accessed from multiple threads and must always
+        // be protected by taking `interface.locker`.
         submitted: Packet.Queue,
         pending: Packet.Queue,
 
@@ -387,9 +389,11 @@ pub fn ContextType(
                 self.packet_cancel(packet);
             }
 
-            // The submitted queue is no longer accessible to user threads,
-            // so synchronization is not required here.
-            while (self.submitted.pop()) |packet| {
+            while (brk: {
+                self.interface.locker.lock();
+                defer self.interface.locker.unlock();
+                break :brk self.submitted.pop();
+            }) |packet| {
                 packet.assert_phase(.submitted);
                 self.packet_cancel(packet);
             }
@@ -905,7 +909,11 @@ pub fn ContextType(
             self.signal.stop();
             self.thread.join();
 
-            assert(self.submitted.pop() == null);
+            {
+                self.interface.locker.lock();
+                defer self.interface.locker.unlock();
+                assert(self.submitted.pop() == null);
+            }
             assert(self.pending.pop() == null);
 
             self.gpa.allocator().free(self.addresses_copy);
