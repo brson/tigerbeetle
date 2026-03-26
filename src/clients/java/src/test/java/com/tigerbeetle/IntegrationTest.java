620ea79270647a21a2eb4586093ef043217fb107
Brian Anderson <andersrb@gmail.com>
2026-03-26 17:56:59 +0000

    tb_client: Don't early-shutdown on eviction
    Fixes a variety of issues described in the context.zig module docs.

--- src/clients/java/src/test/java/com/tigerbeetle/IntegrationTest.java
+++ src/clients/java/src/test/java/com/tigerbeetle/IntegrationTest.java
@@ -1033,12 +1033,13 @@ public class IntegrationTest {
                     assertEquals(PacketStatus.ClientEvicted.value, requestException.getStatus());
                 }
 
-                // The client is deinitialized after it learns it was evicted.
-                // Reusing an evicted client must return a "ClientShutdown" error.
+                // After eviction, subsequent submissions are accepted but
+                // complete with the eviction error.
                 try {
                     client_evict.lookupAccounts(new IdBatch(UInt128.id()));
                     assert false;
-                } catch (ClientClosedException clientClosedException) {
+                } catch (RequestException requestException) {
+                    assertEquals(PacketStatus.ClientEvicted.value, requestException.getStatus());
                 }
             }
         }
